3ba936f8d9b22bd54031a6605c5f3ae63c1e1d14
B Manke <brent.a.manke@gmail.com>
2024-10-23 03:35:14 +0000

    Oct 22, 2024, 9:35 PM

--- src/data/initialRecipes.ts
+++ src/data/initialRecipes.ts
@@ -76,5 +76,18 @@ export const initialRecipes: Recipe[] = [
         image: "https://images.unsplash.com/photo-1561043433-aaf687c4cf04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
         mealType: "Appetizer",
         rating: 5
+    },
+    {
+    id: 6,
+        name: "Greek Salad",
+        ingredients: ["¼ cup extra-virgin olive oil", "3 tablespoons red wine vinegar", "1 garlic clove, minced", "½ teaspoon dried oregano, more for sprinkling", "¼ teaspoon Dijon mustard", "¼ teaspoon sea salt", "Freshly ground black pepper", "1 English cucumber, cut lengthwise, seeded, and sliced ¼-inch thick", "1 green bell pepper, chopped into 1-inch pieces", "2 cups halved cherry tomatoes", "5 ounces feta cheese, cut into ½ inch cubes*", "⅓ cup thinly sliced red onion", "⅓ cup pitted Kalamata olives", "⅓ cup fresh mint leaves"],
+        steps: [
+            "Make the dressing: In a small bowl, whisk together the olive oil, vinegar, garlic, oregano, mustard, salt, and several grinds of pepper.",
+            "On a large platter, arrange the cucumber, green pepper, cherry tomatoes, feta cheese, red onions, and olives. Drizzle with the dressing and very gently toss. Sprinkle with a few generous pinches of oregano and top with the mint leaves. Season to taste and serve.",
+            "Serve cold and enjoy your refreshing Greek salad!"
+        ],
+        image: "https://images.unsplash.com/photo-1561043433-aaf687c4cf04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+        mealType: "Appetizer",
+        rating: 5
     }
 ];
